7d2248a8f9c4440d12fbea553bd5ec9529461551
Baktybeks <baktybeks@mail.ru>
2025-06-26 02:37:02 +0600

    merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+// src/app/layout.tsx
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import type { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BAMORA",
+  title: "Student Management System",
   description: "Платформа для управления академической деятельностью",
 };
 
@@ -13,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
-      <body>
+    <html lang="ru" suppressHydrationWarning={true}>
+      <body className={inter.className} suppressHydrationWarning={true}>
         <Providers>{children}</Providers>
       </body>
     </html>
